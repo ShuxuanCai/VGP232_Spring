200b76ccbfe8da59a2f1d543c4f11f8eee8401e2
ShuxuanCai <caishuxuan00@gmail.com>
2021-05-27 19:02:31 -0700

    Week 6

---
+++ VGP232_Spring/Week_6/Crypto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Week_6
+{
+    public enum CryptoAlgorithm { AES, RSA };
+    public class Crypto
+    {
+        AesCryptoServiceProvider aes;
+        //RSACryptoServiceProvider res;
+
+        CryptoAlgorithm mType;
+
+
+        public void Initialize(CryptoAlgorithm type)
+        {
+            mType = type;
+            //if
+            aes = new AesCryptoServiceProvider();
+        }
+
+        public void Terminate()
+        {
+            if(aes != null)
+            {
+                aes.Dispose();
+            }
+
+            //Do the same for rsa
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            if(mType == CryptoAlgorithm.AES)
+            {
+                if(data == null || data.Length == 0)
+                {
+                    return null;
+                }
+
+                using(var ms = new MemoryStream())
+                {
+                    using(var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                        cs.FlushFinalBlock();
+                    }
+                    return ms.ToArray();
+                }
+            }
+            else
+            {
+                //Here you will have to do for RSA
+                return null;
+            }
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            if (mType == CryptoAlgorithm.AES)
+            {
+                if (data == null || data.Length == 0)
+                {
+                    return null;
+                }
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                        cs.FlushFinalBlock();
+                    }
+                    return ms.ToArray();
+                }
+            }
+            else
+            {
+                //Here you will have to do for RSA
+                return null;
+            }
+        }
+    }
+}
---
+++ VGP232_Spring/Week_6/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Crypto crypto;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            crypto = new Crypto();
+            crypto.Initialize(CryptoAlgorithm.AES);
+        }
+
+        private void EncryptPressed(object sender, RoutedEventArgs e)
+        {
+            string plainText = tbPlainText.Text;
+            plainText = plainText.Replace(' ', '+');
+            int padding = plainText.Length % 4;
+            if(padding != 0)
+            {
+                plainText += new string('+', 4 - padding);
+            }
+
+            byte[] data = Convert.FromBase64String(plainText);
+            byte[] cypherData = crypto.Encrypt(data);
+            string cypherText = Convert.ToBase64String(cypherData);
+            tbCypherText.Text = cypherText;
+        }
+
+        private void DecryptPressed(object sender, RoutedEventArgs e)
+        {
+            string cypherText = tbCypherText.Text;
+            byte[] cypherData = Convert.FromBase64String(cypherText);
+            byte[] plainData = crypto.Decrypt(cypherData);
+            string plainText = Convert.ToBase64String(plainData);
+            plainText = plainText.Replace('+', ' ');
+            tbDecypherText.Text = plainText;
+        }
+
+        private void SavePressed(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            if(saveFile.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFile.FileName, tbDecypherText.Text);
+            }
+        }
+    }
+}
